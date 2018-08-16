47881e8d530757cd03bf7b311f49f5f75c114884
Noir-Nuclear <filstormborn@mail.ru>
2018-08-16 18:30:14 +0400

    code refactoring

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../resources/sample.fxml"));
         Scene rootScene = new Scene(root);
 
         primaryStage.setTitle("Rabbit and wolfs");
